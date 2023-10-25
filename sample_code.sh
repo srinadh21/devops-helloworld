de6d3191df041cd2448bf3ec1974edb0beaa8cb1
srinadh21 <144473538+srinadh21@users.noreply.github.com>
2023-10-25 09:54:01 -0400

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -6,4 +6,4 @@ echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
-echo "$SUM"
+echo "$SUM a"
